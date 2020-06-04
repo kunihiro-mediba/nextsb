b3b6cf61d31e01603b5a3ff7feb76ff1284c463a
Ryoh Kunihiro <styrkuni@mediba.jp>
2020-06-04 17:18:01 +0900

    Reformat code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <main>
-        <div>Hello</div>
-      </main>
-    </div>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <main>
+                <div>Hello</div>
+            </main>
+        </div>
+    );
 }
